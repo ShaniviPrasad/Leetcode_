62624f5b5dfc30059d7cc93c1459955304c960fb
Shanivi Prasad <166331340+ShaniviPrasad@users.noreply.github.com>
2026-05-02 19:51:48 +0530

    Time: 3 ms (33.51%) | Memory: 9.7 MB (18.77%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,20 +1,34 @@
+#include <vector>
+using namespace std;
 class Solution {
 public:
-    bool isGood(int num){
-        bool change=false;
-        while(num>0){
-        int d=num%10;
-        if(d==3 || d==4|| d==7) return false;
-        if(d==2 || d==5|| d==6||d==9) change=true;
-        num/=10;
-        }
-        return change;
+    int solve(int num, vector<int>& dp) {
+        if (num == 0) return 0;
+        if (dp[num] != -1) return dp[num];
+        int rem = solve(num / 10, dp);
+        int d = num % 10;
+        int check_d;
+        if (d == 0 || d == 1 || d == 8) 
+            check_d = 0;
+        else if (d == 2 || d == 5 || d == 6 || d == 9) 
+            check_d = 1;
+        else 
+            return dp[num] = 2;
+        if (rem == 2) return dp[num] = 2;
+        if (check_d == 2) return dp[num] = 2;
+
+        if (rem == 1 || check_d == 1)
+            return dp[num] = 1;
+
+        return dp[num] = 0;
     }
     int rotatedDigits(int n) {
-        int count =0;
-        for(int i=0; i<=n; i++){
-            if(isGood(i))
-            count++;
+        vector<int> dp(n + 1, -1);
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (solve(i, dp) == 1) {
+                count++;
+            }
         }
         return count;
     }
