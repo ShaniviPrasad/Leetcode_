b0f24b9bd2721dd9acec4800d2a8419da4a7dae4
Shanivi Prasad <166331340+ShaniviPrasad@users.noreply.github.com>
2026-05-24 22:22:19 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (60.58%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -11,6 +11,7 @@ public:
         for(int i=0; i<n; i++){
             int num=abs(nums[i]);
             int idx=num-1;
+            if(nums[idx]<0) continue;
             if(nums[idx]>0)
                 nums[idx] *=-1;
         }
