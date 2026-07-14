1c963bc6e977c24004037841c99029ea62d14c43
Shanivi Prasad <166331340+ShaniviPrasad@users.noreply.github.com>
2026-07-14 23:59:44 +0530

    Time: 14 ms (18.41%) | Memory: 21.8 MB (15.32%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -6,22 +6,21 @@ public:
         vector<int>suf(n+1);
         pref[0]=0;
         for(int i=0; i<n; i++){
-             pref[i+1]=pref[i]+((customers[i]=='N')?1:0);
+             pref[i+1]=pref[i]+(customers[i]=='N');
         }
         suf[n]=0;
         for(int i=n-1; i>=0; i--){
-             suf[i]=suf[i+1]+((customers[i]=='Y')? 1:0);
+             suf[i]=suf[i+1]+(customers[i]=='Y');
         }
+        int ans=0;
         int minpen=INT_MAX;
         for(int i=0; i<=n; i++){
-            pref[i]+=suf[i];
-            int pen=pref[i];
-            minpen=min(minpen, pen);
+            int pen=pref[i]+suf[i];
+             if(pen<minpen){
+                minpen=pen;
+                ans=i;
+            }
         }
-        for(int i=0; i<n; i++){
-            int pen=pref[i];
-            if(pen==minpen) return i;
-        }
-        return n;
+        return ans;
     }
 };
