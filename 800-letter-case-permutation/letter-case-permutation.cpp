69241e827c0b5308e303655e809e99a544148c1c
Shanivi Prasad <166331340+ShaniviPrasad@users.noreply.github.com>
2026-09-22 02:20:48 +0530

    Time: 4 ms (31.14%) | Memory: 15.4 MB (27.86%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.cpp
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    void solve(string s, string& ans, vector<string>& all){
+    void solve(string s, string ans, vector<string>& all){
         if(s.size()==0){
             all.push_back(ans);
             return;
         }
-        string ans1=ans;
-        string ans2=ans;
-        if(isalpha(s[0])){
-            ans1.push_back(tolower(s[0]));
-            ans2.push_back(toupper(s[0]));
-             s.erase(s.begin());
-            solve(s, ans1, all);
-            solve(s, ans2, all);
+       char ch = s[0];
+       s.erase(s.begin());
+        if(isalpha(ch)){
+            solve(s, ans+(char)tolower(ch),all);
+            solve(s, ans+(char)toupper(ch),all);
         }
         else{
-            ans.push_back(s[0]);
-             s.erase(s.begin());
-            solve(s, ans, all);
+            solve(s, ans+ch, all);
         }
        
     }
